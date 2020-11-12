2352f1957be880644c8ab311491abf517b86ef5f
Winston Chen <winstonchen.wm@gmail.com>
2020-11-12 00:31:43 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,15 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
+
+end
+
+def consolidate_cart(cart)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This returns a new Array that represents the cart. Don't merely
+  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = {} 
   cart.each do |items_array| 
     items_array.each do |item, attribute_hash| 
@@ -12,15 +21,6 @@ def find_item_by_name_in_collection(name, collection)
 end 
 new_cart 
 end
-
-end
-
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
 end
 
 
